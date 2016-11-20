3647de30407450b9b8fa0d9771c5d3ea88dcbc95
Dan Passarelli <danpassarelli@Dans-MacBook-Pro.local>
2016-11-20 14:48:43 -0800

    added nav code to index.html in the angular version of bloc jams

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,13 +84,6 @@ module.exports = function(grunt) {
     
     grunt.registerTask('default', 'Testing out Grunt logging and task callbacks', ['watch']);
     
-    grunt.registerTask('fun', 'This task is for fun only', function() {
-        grunt.log.writeln('This the *fun* Grunt task');
-    });
- 
-    grunt.registerTask('serious', 'This task is for serious stuff only', function() {
-        grunt.log.writeln('Wipe that smirk off your face; this is serious.');
-    });
     
     grunt.initConfig({
         watch: {
@@ -105,6 +98,7 @@ module.exports = function(grunt) {
             }
         }
     });
+                   
             
     
     grunt.loadNpmTasks('grunt-contrib-watch');
