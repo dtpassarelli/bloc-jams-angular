c6ee4af0a3f0516440856576cfa389d505ea4ac9
Dan Passarelli <danpassarelli@Dans-MacBook-Pro.local>
2016-11-27 21:21:59 -0700

    Created fixtures.js in the services directory and added album data

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,16 +1,15 @@
 (function() {
-     function AlbumCtrl() {
+     function AlbumCtrl(Fixtures) {
      	this.albumArt = "/assets/images/album_covers/01.png";
      	this.name = "Turn the Music Up!";
      	this.artist = "Turn the Music Up!";
      	this.yearOfLabel = "Turn the Music Up!";
      	this.albums = [];
-     	for (var i=0; i < 12; i++) {
-         	this.albums.push(angular.copy(albumPicasso));
+     	this.albumData = Fixtures.getAlbum();
      	}
      }
  
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', AlbumCtrl);
+         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
  })();
