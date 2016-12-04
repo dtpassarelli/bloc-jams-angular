33aa07462efd7e28697ae886c1c604aab1e2dbea
Dan Passarelli <danpassarelli@Dans-MacBook-Pro.local>
2016-12-04 13:41:31 -0700

    Created controller for the play bar and updated play and pause methods

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,14 +1,22 @@
 (function() {
-     function SongPlayer() {
-         var SongPlayer = {};
+     function SongPlayer(Fixtures) {
+        var SongPlayer = {};
 
-         var currentSong = null;
+        var currentAlbum = Fixtures.getAlbum();
+
+         
+
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
          /**
  * @desc Buzz object audio file
  * @type {Object}
  */
 
-     	 var currentBuzzObject = null;
+        SongPlayer.currentSong = null;
+
+     	var currentBuzzObject = null;
      	 /**
  * @function setSong
  * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -36,9 +44,10 @@
 
 
          SongPlayer.play = function(song) {
-         	
-         	setSong(song);
-         	playSong();
+         	song = song || SongPlayer.currentSong;
+            if (SongPlayer.currentSong !== song) {
+         	  setSong(song);
+         	  playSong();
 
          } else if (currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
@@ -48,10 +57,24 @@
      	};
 
      	SongPlayer.pause = function(song) {
-		    
+		    song = song || SongPlayer.currentSong;
 		    currentBuzzObject.pause();
 		    song.playing = false;
  		};
+
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+
+            if (currentSongIndex < 0) {
+                currentBuzzObject.stop();
+                SongPlayer.currentSong.playing = null;
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
 
 
           return SongPlayer;
