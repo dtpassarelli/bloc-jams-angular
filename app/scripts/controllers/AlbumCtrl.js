078ff4ee98f47d098def0b9c22cda47a4ec4f864
Dan Passarelli <danpassarelli@Dans-MacBook-Pro.local>
2016-12-20 20:30:04 -0700

    added albumData and edited html

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,13 +1,6 @@
 (function() {
      function AlbumCtrl() {
-     	this.albumArt = "/assets/images/album_covers/01.png";
-     	this.name = "Turn the Music Up!";
-     	this.artist = "Turn the Music Up!";
-     	this.yearOfLabel = "Turn the Music Up!";
-     	this.albums = [];
-     	for (var i=0; i < 12; i++) {
-         	this.albums.push(angular.copy(albumPicasso));
-     	}
+     	this.albumData = angular.copy(albumPicasso);
      }
  
      angular
